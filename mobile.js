84267b4fe5831843c06c0d500ae84cb655e025de
Daniel <2005danielus@gmail.com>
2020-12-20 11:50:17 +0100

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -1,14 +1,12 @@
-navegador = navigator.userAgent; //busco el "userAgent" del usuario.
-//lista de palabras del "userAgent" en los móviles
+navegador = navigator.userAgent;
 moviles = ["Mobile", "iPhone", "iPod", "BlackBerry", "Opera Mini", "Sony", "MOT", "Nokia", "samsung"];
-detector = 0; //Variable que detectará si se usa un móvil
-for (i in moviles) { //comprobar en la lista ...
-    //si el método "indexOf" no devuelve -1, indica que la palabra está en el "userAgent"
+detector = 0;
+for (i in moviles) {
     compruebo = navegador.indexOf(moviles[i]);
     if (compruebo > -1) {
-        detector = 1; //Si es un móvil, cambio el valor del detector
+        detector = 1;
     }
 }
-if (detector == 1) { //si es un móvil redirecciono la página.
-    location.href = "m.html"; //Se redirecciona hacia la versión móvil.
+if (detector == 1) {
+    location.href = "m.html";
 }
